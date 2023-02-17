2f3b76c2124bbd056def210c635309c74ed1953d
Yeansovanvathana <yean.sovanvathana19@kit.edu.kh>
2023-02-17 15:30:12 +0700

    Finish sample otp

--- OTP/app/src/main/java/com/example/otp/MainActivity.java
+++ OTP/app/src/main/java/com/example/otp/MainActivity.java
@@ -10,8 +10,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,15 +47,60 @@ public class MainActivity extends AppCompatActivity {
                 // Get the text that the user entered
                 String inputText = textInputEditText.getText().toString();
 
-                // Show a Toast message with the input text
-                Intent intent = new Intent(MainActivity.this, otp.class);
-
-                // Pass the input text as an extra to the next activity
-                intent.putExtra("input_text", inputText);
-
-                // Start the next activity
-                startActivity(intent);
+//                // Show a Toast message with the input text
+//                Intent intent = new Intent(MainActivity.this, otp.class);
+//
+//                // Pass the input text as an extra to the next activity
+//                intent.putExtra("input_text", inputText);
+//
+//                // Start the next activity
+//                startActivity(intent);
+                loadtext(inputText);
             }
         });
+
+    }
+    private <string> void loadtext(String phone) {
+
+        String url = "http://4f45-119-13-56-108.ngrok.io/otp";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("phone_number", phone);
+
+        JSONObject jsonObject = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        String responseString = response.toString();
+//                        Log.d("API Response", responseString);
+                        try {
+                            int code = response.getInt("code");
+                            String phone_number = response.getString("phone_number");
+                            Log.d("API Response", "Code: " + code);
+                            Log.d("API Response", "Phone_number: " + phone_number);
+
+                            String codeStr = String.valueOf(code);
+                            Intent intent = new Intent(MainActivity.this, otp.class);
+//
+                            intent.putExtra("code", codeStr);
+                            intent.putExtra("phone_number", phone_number);
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors
+                    }
+                }
+        );
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
 }
--- OTP/app/src/main/java/com/example/otp/otp.java
+++ OTP/app/src/main/java/com/example/otp/otp.java
@@ -2,11 +2,13 @@ package com.example.otp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.chaos.view.PinView;
 import com.example.otp.R;
 
 public class otp extends AppCompatActivity {
@@ -17,14 +19,28 @@ public class otp extends AppCompatActivity {
         setContentView(R.layout.otp_verification);
 
         // Find the TextView by ID
-        TextView textView = findViewById(R.id.otp_number);
-
+        TextView textView = findViewById(R.id.otp_code);
+        TextView textView_Number = findViewById(R.id.otp_number);
+        PinView pinView = findViewById(R.id.pin_view);
         // Get the input text from the previous activity's extra
         Intent intent = getIntent();
-        String inputText = intent.getStringExtra("input_text");
 
+        String PinCode = intent.getStringExtra("code");
+        String phone_number = intent.getStringExtra("phone_number");
         // Set the input text in the TextView
-        textView.setText(inputText);
+        textView.setText(PinCode);
+        textView_Number.setText(phone_number);
+//        pinView.setText(PinCode);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < PinCode.length(); i++) {
+                    char c = PinCode.charAt(i);
+                    pinView.setText(pinView.getText().append(Character.toString(c)));
+                }
+            }
+        }, 2000);
 
         Button submitButton = findViewById(R.id.confirm_button);
 
